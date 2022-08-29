349e23416f820ff1b9a98bf12f1fdf5e6af6c352
olamileke <olamileke.f@gmail.com>
2022-08-30 00:26:20 +0100

    feat: continued work on home page

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -9,9 +9,9 @@ export const About = () => {
     <Box bg="ivory" color="ivory" boxSizing="border-box" w="100vw">
       <SimpleGrid bg="charlestonGreen" px={28} pt={20} pb={16} columns={12}>
         <GridItem colSpan={5}>
-          <VStack spacing={8} align="flex-start">
+          <VStack spacing={10} align="flex-start">
             <Heading
-              fontSize="1.7rem"
+              fontSize="1.61rem"
               lineHeight="1.575"
               textShadow="0.5px 0.5px navajoWhite"
             >
