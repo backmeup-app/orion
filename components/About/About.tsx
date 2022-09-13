6c8c5d0917417c6adc7a15d43d5ee7ebe025f46a
olamileke <olamileke.f@gmail.com>
2022-09-13 19:44:45 +0100

    feat: working in about section

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,33 +1,42 @@
 import { useState } from "react";
-import { Box, Heading, SimpleGrid, GridItem, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Container,
+  SimpleGrid,
+  GridItem,
+  VStack,
+} from "@chakra-ui/react";
 import { Features, Slides } from "..";
 
 export const About = () => {
   const [selectedFeature, setSelectedFeature] = useState(0);
 
   return (
-    <Box bg="ivory" color="ivory" boxSizing="border-box" w="100vw">
-      <SimpleGrid bg="charlestonGreen" px={28} pt={20} pb={16} columns={12}>
-        <GridItem colSpan={5}>
-          <VStack spacing={10} align="flex-start">
-            <Heading
-              fontSize="2xl"
-              lineHeight="1.575"
-              textShadow="0.5px 0.5px navajoWhite"
-            >
-              Lorem ipsum dolor sit amet, consectetur.
-            </Heading>
-            <Features
-              selectedFeatureIndex={selectedFeature}
-              handleFeatureClick={setSelectedFeature}
-            />
-          </VStack>
-        </GridItem>
-        <GridItem colSpan={1}></GridItem>
-        <GridItem colSpan={6} d="flex" justifyContent="center">
-          <Slides selectedFeatureIndex={selectedFeature} />
-        </GridItem>
-      </SimpleGrid>
+    <Box bg="charlestonGreen" color="ivory" pt={20} pb={16}>
+      <Container w="90%" margin="0 auto" maxW="1350px">
+        <SimpleGrid columns={12}>
+          <GridItem colSpan={{ base: 12, lg: 5 }}>
+            <VStack spacing={10} align="flex-start">
+              <Heading
+                fontSize="2xl"
+                lineHeight="1.575"
+                textShadow="0.5px 0.5px navajoWhite"
+              >
+                Lorem ipsum dolor sit amet, consectetur.
+              </Heading>
+              <Features
+                selectedFeatureIndex={selectedFeature}
+                handleFeatureClick={setSelectedFeature}
+              />
+            </VStack>
+          </GridItem>
+          <GridItem colSpan={{ base: 0, lg: 1 }}></GridItem>
+          <GridItem colSpan={{ base: 12, lg: 5 }}>
+            <Slides selectedFeatureIndex={selectedFeature} />
+          </GridItem>
+        </SimpleGrid>
+      </Container>
     </Box>
   );
 };
