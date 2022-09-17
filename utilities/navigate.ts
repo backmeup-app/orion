2bd3e4547106ca2589131349b10b724ba49aba0e
olamileke <olamileke.f@gmail.com>
2022-09-17 19:33:14 +0100

    chore: added admin url fallback

--- utilities/navigate.ts
+++ utilities/navigate.ts
@@ -1,6 +1,8 @@
 export const navigate = (route: string) => {
   const anchor = window.document.createElement("a");
-  anchor.href = (process.env.NEXT_PUBLIC_ADMIN_URL as string) + route;
+  anchor.href =
+    ((process.env.NEXT_PUBLIC_ADMIN_URL as string) ??
+      "https://dome-admin.olamileke.me") + route;
   anchor.target = "_blank";
   anchor.click();
 };
